d69f5b3c4e1ee383255648bb1ede4594e3200892
Ruslan Agishev <agishrus@fel.cvut.cz>
2025-06-25 09:58:35 +0200

    learning disparity correction term with input normalization

--- scripts/disp_correction.py
+++ scripts/disp_correction.py
@@ -53,6 +53,8 @@ class Data(Dataset):
     """
     A dataset for disparity correction.
     """
+    mean_gray = 0.3611
+    std_gray = 0.2979
 
     def __init__(self, path, max_disp=100.0):
         super(Dataset, self).__init__()
@@ -99,6 +101,27 @@ class Data(Dataset):
         stamp = float(ind.replace('_', '.'))
         return stamp
 
+    def calculate_img_stats(self):
+        """
+        Calculate mean and standard deviation of grayscale images in the dataset.
+        """
+        mean_gray = 0.0
+        std_gray = 0.0
+        n_pixels = 0
+
+        for i in tqdm(range(len(self)), desc='Calculating image stats'):
+            gray = self.get_image(i)
+            gray = gray / 255.0  # normalize to 0-1 if needed
+            mean_gray += gray.sum().item()
+            std_gray += (gray ** 2).sum().item()
+            n_pixels += gray.size
+
+        mean_gray /= n_pixels
+        std_gray = (std_gray / n_pixels - mean_gray ** 2) ** 0.5
+        self.mean_gray = mean_gray
+        self.std_gray = std_gray
+        print(f'Mean gray: {self.mean_gray:.4f}, Std gray: {self.std_gray:.4f}')
+
     def get_image(self, i, camera='left'):
         img_path = self.image_files[camera][i]
         img = Image.open(img_path)
@@ -109,6 +132,32 @@ class Data(Dataset):
         disp_path = self.disp_files[source][i]
         disp = np.load(disp_path)
         return disp
+
+    def disp_to_cloud(self, disp):
+        """
+        Convert disparity map to point cloud.
+        :param disp: Disparity map (H, W)
+        :return: Point cloud (N, 3)
+        """
+        K = np.array(self.calib['camera_left']['camera_matrix']['data']).reshape(3, 3)
+        f = K[0, 0]  # focal length in pixels
+        Tr_camera_left__robot = np.array(self.calib['transformations']['Tr_camera_left__robot']['data'], dtype=float).reshape(4, 4)
+        Tr_camera_right__robot = np.array(self.calib['transformations']['Tr_camera_right__robot']['data'], dtype=float).reshape(4, 4)
+        B = np.linalg.norm(Tr_camera_left__robot[:3, 3] - Tr_camera_right__robot[:3, 3])  # [m]
+        valid_mask = disp > 0
+        depth = np.zeros_like(disp, dtype=np.float32)
+        depth[valid_mask] = (B * f) / disp[valid_mask]
+        height, width = disp.shape
+        vu = np.indices(disp.shape).reshape((2, -1))  # shape (2, H*W)
+        uv = vu[::-1, :]  # shape (2, H*W)
+        coords = np.vstack([
+            uv[0, :],  # u (x-coordinate)
+            uv[1, :],  # v (y-coordinate)
+            np.ones(height * width)
+        ])
+        K_inv = np.linalg.inv(K)
+        points = (K_inv @ coords) * depth.flatten()  # shape (3, H*W)
+        return points.T
 
     def get_sample(self, i):
         img = self.get_image(i, camera='right')
@@ -122,7 +171,8 @@ class DCModel(torch.nn.Module):
         super().__init__()
         self.model = smp.Linknet(
             encoder_name='mobilenet_v2',
-            encoder_weights='imagenet',
+            # encoder_weights='imagenet',
+            encoder_weights=None,  # No pre-trained weights, as we use grayscale images as input
             in_channels=2,
             classes=1,
             activation=None,
@@ -174,6 +224,7 @@ def train(args):
     nepochs = args.nepochs
 
     ds = Data(dataset_path)
+    ds.calculate_img_stats()
     loader = DataLoader(ds, batch_size=bs, shuffle=True)
 
     model = DCModel()
@@ -190,8 +241,13 @@ def train(args):
             disp_in = disp_in.float().to(device)
             disp_gt = disp_gt.float().to(device)
 
+            # normalize input images
+            img_in = (img_in - ds.mean_gray) / ds.std_gray
+            disp_in /= ds.max_disp
+
             optimizer.zero_grad()
-            disp_pred = model(torch.cat([img_in, disp_in], dim=1))
+            disp_corr = model(torch.cat([img_in, disp_in], dim=1))
+            disp_pred = disp_in + disp_corr
 
             mask_dist = (disp_in > 0) & (disp_gt < ds.max_disp)
             mask_nan = torch.isnan(disp_gt) | torch.isnan(disp_gt)
@@ -200,7 +256,7 @@ def train(args):
             mask_valid[..., :, :7] = False
             mask = mask_dist & mask_valid & (~mask_nan)
 
-            loss = criterion(disp_pred[mask] / ds.max_disp, disp_gt[mask] / ds.max_disp)
+            loss = criterion(disp_pred[mask], disp_gt[mask])
             loss_epoch += loss.item()
 
             loss.backward()
@@ -216,9 +272,13 @@ def train(args):
 
 
 def result(args):
+    import open3d as o3d
+
     dataset_path = args.dataset_path
     device = args.device
 
+    ds = Data(dataset_path)
+    # ds.calculate_img_stats()
     loader = DataLoader(Data(dataset_path), batch_size=1, shuffle=True)
 
     model = DCModel()
@@ -226,26 +286,54 @@ def result(args):
     model.eval()
     model.to(device)
 
-    # visualize predictions
     with torch.no_grad():
         img_in, disp_in, disp_gt = next(iter(loader))
         img_in = img_in.to(device)
         disp_in = disp_in.float().to(device)
-        disp_pred = model(torch.cat([img_in, disp_in], dim=1))
-        disp_pred = disp_pred.cpu().numpy()[0][0]
-        disp_gt = disp_gt.cpu().numpy()[0][0]
+
+        # normalize input images
+        img_in_norm = (img_in - ds.mean_gray) / ds.std_gray
+        disp_in_norm = disp_in / ds.max_disp
+
+        disp_corr = model(torch.cat([img_in_norm, disp_in_norm], dim=1))
+        disp_pred = disp_in + disp_corr
 
         # visualize colored disparities
-        disp_scaled = cv2.convertScaleAbs(disp_pred, alpha=255.0 / disp_pred.max())
+        disp_pred = disp_pred.cpu().numpy()[0][0]
+        disp_gt = disp_gt.cpu().numpy()[0][0]
+        disp_in = disp_in.cpu().numpy()[0][0]
+
+        disp_in_scaled = cv2.convertScaleAbs(disp_in, alpha=255.0 / ds.max_disp)
+        disp_in_colored = cv2.applyColorMap(disp_in_scaled, cv2.COLORMAP_JET)
+        cv2.imshow("Disparity Input", disp_in_colored)
+
+        disp_scaled = cv2.convertScaleAbs(disp_pred, alpha=255.0 / ds.max_disp)
         disp_colored = cv2.applyColorMap(disp_scaled, cv2.COLORMAP_JET)
         cv2.imshow("Disparity Prediction", disp_colored)
 
-        disp_scaled_gt = cv2.convertScaleAbs(disp_gt, alpha=255.0 / disp_gt.max())
+        disp_scaled_gt = cv2.convertScaleAbs(disp_gt, alpha=255.0 / ds.max_disp)
         disp_colored_gt = cv2.applyColorMap(disp_scaled_gt, cv2.COLORMAP_JET)
         cv2.imshow("Disparity Ground Truth", disp_colored_gt)
 
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+
+        points = ds.disp_to_cloud(disp_pred)
+        points_gt = ds.disp_to_cloud(disp_gt)
+        valid_mask = disp_in > 0
+        points = points[valid_mask.flatten()]
+        points_gt = points_gt[valid_mask.flatten()]
+
+        # visualize point clouds
+        pcd_pred = o3d.geometry.PointCloud()
+        pcd_pred.points = o3d.utility.Vector3dVector(points)
+        pcd_pred.paint_uniform_color([0, 1, 0])  # green
+
+        pcd_gt = o3d.geometry.PointCloud()
+        pcd_gt.points = o3d.utility.Vector3dVector(points_gt)
+        pcd_gt.paint_uniform_color([1, 0, 0])  # red
+
+        o3d.visualization.draw_geometries([pcd_pred, pcd_gt])
 
 
 def inference_test(agrs):
